68132d9e77cbbec6114aa279f702c46e1e2fa7a1
filipe1309 <filipe1309@outlook.com>
2019-03-24 22:13:56 +0000

    Challenge 25
    Signed-off-by: filipe1309 <filipe1309@outlook.com>

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -1,16 +1,43 @@
-/*
-Essa semana você terá dois desafios:
-1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
-tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
-ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
-o que não ficou tão claro das aulas anteriores.
-É essencial que você entenda todo o conteúdo que foi passado até aqui,
-para que possamos prosseguir para a parte mais avançada do curso :D
-
-2) Estudar eventos!
-Acesse a página do MDN:
-https://developer.mozilla.org/en-US/docs/Web/Events#Categories
-
-Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
-desafio os experimentos legais que você conseguir desenvolver :D
-*/
+(function(win, doc) {
+    'use strict';
+    
+    /*
+    Essa semana você terá dois desafios:
+    1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
+    tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
+    ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
+    o que não ficou tão claro das aulas anteriores.
+    É essencial que você entenda todo o conteúdo que foi passado até aqui,
+    para que possamos prosseguir para a parte mais avançada do curso :D
+    
+    2) Estudar eventos!
+    Acesse a página do MDN:
+    https://developer.mozilla.org/en-US/docs/Web/Events#Categories
+    
+    Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
+    desafio os experimentos legais que você conseguir desenvolver :D
+    */
+    
+    function initialize() {
+        initEvents();
+    }
+    
+    function initEvents() {
+        doc.querySelector('[data-js="focus"]')
+        .addEventListener('focus', function (event) {
+            console.log('Focus!');
+        });
+        
+        doc.querySelector('[data-js="mouseover"]')
+        .addEventListener('mouseover', function (event) {
+            console.log('Mouse Over!');
+        });
+        
+        doc.querySelector('[data-js="dblclick"]')
+        .addEventListener('dblclick', function (event) {
+            console.log('Double Click!');
+        });
+    }
+    
+    initialize();
+})(window, document);
