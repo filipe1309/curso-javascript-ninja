d83b9a33e7b2d3b8e6cd50b8ccb2a1419de96c87
filipe1309 <filipe1309@outlook.com>
2019-03-28 22:08:25 +0000

    Update Challenge 27
    Signed-off-by: filipe1309 <filipe1309@outlook.com>

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,43 +1,45 @@
 (function(win, doc) {
   'use strict';
 
-    /*
-    Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
-    métodos semelhantes aos que existem no array, mas que sirvam para os
-    elementos do DOM selecionados.
-    Crie os seguintes métodos:
-    - forEach, map, filter, reduce, reduceRight, every e some.
-    
-    Crie também métodos que verificam o tipo do objeto passado por parâmetro.
-    Esses métodos não precisam depender de criar um novo elmento do DOM, podem
-    ser métodos estáticos.
-    
-    Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
-    no objeto, como nos exemplos abaixo:
-    DOM.isArray([1, 2, 3]); // true
-    DOM.isFunction(function() {}); // true
-    DOM.isNumber('numero'); // false
-    
-    Crie os seguintes métodos para verificação de tipo:
-    - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
-    O método isNull deve retornar `true` se o valor for null ou undefined.
-    */
-    
-    function DOM(nodeSelector) {
-        this.element = doc.querySelectorAll(nodeSelector);
-        
-        this.on = function (event, callback) {
-          Array.prototype.forEach.call(this.element, function(nodeItem) {
-            nodeItem.addEventListener(event, callback, false);
-          });
-        }
-        
-        this.off = function (event, callback) {
-          Array.prototype.forEach.call(this.element, function(nodeItem) {
-            nodeItem.removeEventListener(event, callback);
-          });
-        }
-    }
+  /*
+  Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
+  métodos semelhantes aos que existem no array, mas que sirvam para os
+  elementos do DOM selecionados.
+  Crie os seguintes métodos:
+  - forEach, map, filter, reduce, reduceRight, every e some.
+  
+  Crie também métodos que verificam o tipo do objeto passado por parâmetro.
+  Esses métodos não precisam depender de criar um novo elmento do DOM, podem
+  ser métodos estáticos.
+  
+  Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
+  no objeto, como nos exemplos abaixo:
+  DOM.isArray([1, 2, 3]); // true
+  DOM.isFunction(function() {}); // true
+  DOM.isNumber('numero'); // false
+  
+  Crie os seguintes métodos para verificação de tipo:
+  - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+  O método isNull deve retornar `true` se o valor for null ou undefined.
+  */
+  
+  // API DOM
+  
+  function DOM(nodeSelector) {
+      this.element = doc.querySelectorAll(nodeSelector);
+      
+      this.on = function (event, callback) {
+        Array.prototype.forEach.call(this.element, function(nodeItem) {
+          nodeItem.addEventListener(event, callback, false);
+        });
+      }
+      
+      this.off = function (event, callback) {
+        Array.prototype.forEach.call(this.element, function(nodeItem) {
+          nodeItem.removeEventListener(event, callback);
+        });
+      }
+  }
   
   DOM.prototype.get = function () {
     return this.element;
@@ -105,9 +107,12 @@
     return 'Test!!!!!!!';
   }
   
+  
+  
+  
+  
+  
   // Testes
-  
-  
   
   var $a = new DOM('[data-js="link"]');
   $a.on('click', function handleClick(e) {
@@ -130,7 +135,7 @@
   var filterValue = $a.filter(function(item) { 
     return Number(item.getAttribute('data-js')) > 1;
   });
-  console.log('filter', );
+  console.log('filter', filterValue);
   
   var reduceValue = $a.reduce(function(accumulator, currentValue) {
       return Number(accumulator.getAttribute('data-js')) + Number(currentValue.getAttribute('data-js'));
