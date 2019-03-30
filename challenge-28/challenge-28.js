d9ac4679a0eb5eb8c00edd914e5fd1d5f7476080
filipe1309 <filipe1309@outlook.com>
2019-03-30 20:52:01 +0000

    Improve Challenge 28
    Signed-off-by: filipe1309 <filipe1309@outlook.com>

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -114,63 +114,90 @@
   
   // Challenge 28
   
-  var $submitButton = new DOM('input[type="submit"]');
+  var $submitButton = new DOM('button[type="submit"]');
   var $cepInput = new DOM('input[name="cep"]');
   var $statusSpan = new DOM('span[data-js="status"]');
+  var ajax = new XMLHttpRequest();
   var $cepSpan = new DOM('span[data-js="cep"]');
   var $logradouroSpan = new DOM('span[data-js="logradouro"]');
   var $bairroSpan = new DOM('span[data-js="bairro"]');
   var $estadoSpan = new DOM('span[data-js="estado"]');
   var $cidadeSpan = new DOM('span[data-js="cidade"]');
-  var ajax = new XMLHttpRequest();
-  var ajaxStates = {
-    //0: function() {}, // not send
-    //1: function() {}, // open
-    //2: function() {}, // headers received
-    3: function(cep) { return 'Buscando informações para o CEP [CEP]...'.replace('[CEP]', cep); }, // loading
-    4: function(cep, cepStatus) {
-      if (cepStatus) {
-        return 'Endereço referente ao CEP [CEP]:'.replace('[CEP]', cep);
-      }
-      return "Não encontramos o endereço para o CEP [CEP].".replace('[CEP]', cep);
-    }// finished
-  };
+  
+  function replaceCep(message, cep) {
+    return message.replace('[CEP]', cep);
+  }
+  
+  function getMessage(type) {
+    //debugger;
+    return {
+      //0: function() {}, // not send
+      //1: function() {}, // open
+      //2: function() {}, // headers received
+      3: function(cep) { return replaceCep('Buscando informações para o CEP [CEP]...', cep); }, // loading
+      4: function(cep, cepStatus) {
+        if (cepStatus) {
+          return replaceCep('Endereço referente ao CEP [CEP]:', cep);
+        }
+        return replaceCep("Não encontramos o endereço para o CEP [CEP].", cep);
+      }// finished
+    }[type];
+  }
   
   function cleanCpf(cpf) {
     return cpf.replace(/\D/g, '');
   }
   
-  function fillForm(cepJson) {
+  function fillCepData(cepJson) {
     $cepSpan.element[0].firstChild.nodeValue = cepJson.code;
-    $logradouroSpan.element[0].firstChild.nodeValue = cepJson.address;
-    $bairroSpan.element[0].firstChild.nodeValue = cepJson.district;
-    $estadoSpan.element[0].firstChild.nodeValue = cepJson.state;
-    $cidadeSpan.element[0].firstChild.nodeValue = cepJson.city;
+    $logradouroSpan.element[0].textContent = cepJson.address;
+    $bairroSpan.element[0].textContent = cepJson.district;
+    $estadoSpan.element[0].textContent = cepJson.state;
+    $cidadeSpan.element[0].textContent = cepJson.city;
   }
   
   function updateStatus(ajaxReadyState, cepStatus, cep) {
-    $statusSpan.element[0].firstChild.nodeValue = ajaxStates[ajaxReadyState](cep, cepStatus);
+    $statusSpan.element[0].firstChild.nodeValue = getMessage(ajax.readyState)(cep, cepStatus);
   }
   
   function isStatusOk(status) {
     return (status === 200);
   }
   
-  ajax.addEventListener('readystatechange', function () {
-    if (isStatusOk(ajax.status) && ajaxStates[ajax.readyState]) {
-      var cepJson = JSON.parse(ajax.responseText);
+  function parseData(data) {
+    var result;
+    try {
+      result = JSON.parse(data);
+    } catch(e) {
+      result = null;
+    }
+    return result;
+    
+  }
+  
+  function handleReadyStateChange() {
+    if (isStatusOk(ajax.status) && getMessage(ajax.readyState)) {
+      var cepJson = parseData(ajax.responseText);
+      if (!cepJson)
+        return;
       updateStatus(ajax.readyState, cepJson.status, cleanCpf($cepInput.get()[0].value));
-      if (ajax.readyState) {
-        fillForm(cepJson);
+      if (ajax.readyState === 4) {
+        fillCepData(cepJson);
       }
     }
-  })
+  }
   
-  $submitButton.on('click', function(event) {
+  function handleClickSubmitButton(event) {
     event.preventDefault();
     var cep = cleanCpf($cepInput.get()[0].value);
     ajax.open('GET', '//apps.widenet.com.br/busca-cep/api/cep.json?code=' + cep);
     ajax.send();
-  });
+  }
+  
+  ajax.addEventListener('readystatechange', handleReadyStateChange);
+  $submitButton.on('click', handleClickSubmitButton);
+  /*
+  OU $formCep.on('submit', callback);
+  */
   
 })(window, document);
